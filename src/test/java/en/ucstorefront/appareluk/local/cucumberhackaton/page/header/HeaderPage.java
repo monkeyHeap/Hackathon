c6839f4237e3e650289ea4a1a92a88212079fcf5
bnbn131 <40466659+bnbn131@users.noreply.github.com>
2021-09-18 15:22:45 +0300

    HeaderPage add

--- src/test/java/en/ucstorefront/appareluk/local/cucumberhackaton/page/header/HeaderPage.java
+++ src/test/java/en/ucstorefront/appareluk/local/cucumberhackaton/page/header/HeaderPage.java
@@ -1,11 +1,20 @@
 package en.ucstorefront.appareluk.local.cucumberhackaton.page.header;
 
+import en.ucstorefront.appareluk.local.cucumberhackaton.base.Base;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class HeaderPage {
+
+    protected WebDriver driver = Base.getDefaultDriver();
+
+    public HeaderPage() {
+        PageFactory.initElements(driver, this);
+    }
 
     @FindBy(xpath = "//*[@class=\"liOffcanvas\"]/a")
     private WebElement gotoLoginButton;
